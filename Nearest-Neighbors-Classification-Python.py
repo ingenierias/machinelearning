374c8e8e30abd5a302f3f9148414d7f9499893aa
Ingeniero <eduardo@webean.org>
2015-09-27 20:18:11 -0700

    Performance
    Se agrego función para identificar el performance del script, del 100% de los datos toma el 70% y realiza las pruebas con el restante, para cambiar y probar otros porcentajes sólo tienes que cambiar la variable "limiar"

--- Nearest-Neighbors-Classification-Python.py
+++ Nearest-Neighbors-Classification-Python.py
@@ -25,7 +25,7 @@ def splitDataset(dataset, splitRatio):
 def euclidiana(valor1, valor2):
 	distance = 0
 	n_features = len(valor2)
-	for x in range(n_features):
+	for x in range(n_features-1):
 		distance += pow(float(valor1[x]) - float(valor2[x]), 2)
 	return math.sqrt(distance)
 
@@ -38,19 +38,20 @@ def vecino(dataset,prueba):
 	distances.sort(key=operator.itemgetter(1))
 	return distances
 
-def getAccuracy(testSet, predictions):
-	correct = 0
-	for x in range(len(testSet)):
-		if testSet[x][-1] is predictions[x]:
-			correct += 1
-	return (correct/float(len(testSet))) * 100.0
-	
+def entrenar(testData,datos_entrenamiento,):
+	performance = 0
+	for n in range(len(testData)):
+		distancias = vecino(datos_entrenamiento,testData[n])
+		if testData[n][-1] == distancias[0][0][-1]:
+			performance +=1
+	return (performance/float(len(testData)) * 100)
+
 def main():
 	archivo = 'iris.data'
 	limiar = 0.7
 	datos = loadCsv(archivo)
 	datos_entrenamiento, testData = splitDataset(datos,limiar)
-	prueba_single = [4.7, 3.2, 1.3, 0.2]#iris
-	distancias = vecino(datos_entrenamiento,prueba_single)
-	print distancias
+	prueba_single = [4.7, 3.2, 1.3, 0.2,'Iris-setosa']#iris
+	performance = entrenar(testData,datos_entrenamiento)
+	print performance	
 main()
